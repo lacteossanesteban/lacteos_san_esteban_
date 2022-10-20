bd57645368200f3844784987beadd90785ab7f5f
robert-virtual <robertocastill63@gmail.com>
2022-10-20 11:16:33 -0600

    sections height

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
-const observer = new IntersectionObserver((entries)=>{
-  entries.forEach(entry=>{
-    if (entry.isIntersecting) {
-     entry.target.classList.add("show") 
-      return
-    }
-     entry.target.classList.remove("show") 
-  })
-})
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        return;
+      }
+      entry.target.classList.remove("show");
+    });
+  },
+);
 
-
-const hiddenElements = document.querySelectorAll(".hidden")
-hiddenElements.forEach(e=>observer.observe(e))
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((e) => observer.observe(e));
